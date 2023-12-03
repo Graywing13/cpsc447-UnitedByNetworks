61f74b5c4239f3935fb46d7afc16338c08fb87fc
UBC Student <dashdot@students.cs.ubc.ca>
2023-12-02 16:39:12 -0800

    Explain SES, edit some terms (#69)
    * Explain SES, edit some terms
    
    * Consolidate scatterplots into 1 file
    
    ---------
    
    Co-authored-by: dashdot <30674558+Graywing13@users.noreply.github.com>

--- js/charts/small-multiples-scatterplots.js
+++ js/charts/small-multiples-scatterplots.js
@@ -16,42 +16,42 @@ const categoriesOfInterest = [
 ];
 
 const categoryNameAndDescription = new Map([
-    ['mean_students_per_cohort', 
-        ['# students per cohort', 
-         'Average number of students per cohort, measured by 10000s']],
-    ['ec_own_ses_college', 
-        ['Low SES x high SES friendships', 
-         'How often low-SES students are friends with high-SES students (a higher value indicates more friendships with high-SES students)']],
-    ['ec_high_parent_ses_college', 
-        ['Both high family SES friendships', 
-         'How often students from high-SES families are friends with other students also from high-SES families (a higher value indicates more friendships with other students of similar high-SES families)']],
-    ['exposure_own_ses_college', 
-        ['Low x high SES exposure to each other', 
-         'Measure of how often low SES students have contact with high SES students (a higher value indicates that students with low SES are interacting more with students with high SES)']],
-    ['exposure_parent_ses_college', 
-        ['Low x high family SES exposure', 
-         'Measure of how often students from families of low SES have contact with students from families of high SES (a higher value indicates that students from lower SES families are interacting more with students from higher SES families)']],
-    ['bias_own_ses_college', 
-        ['Likelihood befriending same SES student', 
-         'Student tendency to be friends with other students who have a similar SES (i.e a higher bias indicates that students are more likely to be friends with other students of similar economic backgrounds)']],
-    ['bias_parent_ses_college', 
-        ['Likelihood befriending same family SES', 
-         'Student tendency to be friends with other students who have parents with similar SES (i.e a higher bias indicates that students are more likely to be friends with other students from families with similar economic backgrounds)']],
-    ['bias_high_own_ses_college', 
-        ['Likelihood befriending high SES student', 
-         'Student tendency to befriend other students with high-SES (a higher bias indicates that students are more likely to be friends with other students of high economic backgrounds)']],
-    ['bias_high_parent_ses_college', 
-        ['Likelihood befriending high family SES', 
-         'Students of high-SES families tendency to befriend other students who also have high-SES families (a higher bias indicates that students with families from higher economic backgrounds are more likely to befriend other students also from high-SES families)']],
-    ['clustering_college', 
-        ['Friend cluster formation', 
-         'The fraction of a student\'s individual friends that are also friends with each other']],
-    ['support_ratio_college', 
-        ['% mutual friends', 
-         'The proportion of friendship pairs that share another mutual friend within the same college']],
-    ['volunteering_rate_college', 
-        ['Student volunteering rate', 
-         'The percentage of Facebook users who are members of a group which is predicted to be about volunteering or activism based on group title and other group characteristics']]
+    ['mean_students_per_cohort',
+        ['# students per cohort (10000s)',
+            'Average number of students per cohort, measured by 10000s']],
+    ['ec_own_ses_college',
+        ['Low SES x high SES friendships',
+            'How often low-SES students are friends with high-SES students (a higher value indicates more friendships with high-SES students)']],
+    ['ec_high_parent_ses_college',
+        ['Frequency of students from high-SES families being friends',
+            'How often students from high-SES families are friends with other students also from high-SES families (a higher value indicates more friendships with other students of similar high-SES families)']],
+    ['exposure_own_ses_college',
+        ['Low SES x high SES contact rate',
+            'Measure of how often low SES students have contact with high SES students (a higher value indicates that students with low SES are interacting more with students with high SES)']],
+    ['exposure_parent_ses_college',
+        ['Low family SES x high family SES contact rate',
+            'Measure of how often students from families of low SES have contact with students from families of high SES (a higher value indicates that students from lower SES families are interacting more with students from higher SES families)']],
+    ['bias_own_ses_college',
+        ['Likelihood of befriending peers with same SES',
+            'Student tendency to be friends with other students who have a similar SES (i.e a higher bias indicates that students are more likely to be friends with other students of similar economic backgrounds)']],
+    ['bias_parent_ses_college',
+        ['Likelihood of befriending peers with same family SES',
+            'Student tendency to be friends with other students who have parents with similar SES (i.e a higher bias indicates that students are more likely to be friends with other students from families with similar economic backgrounds)']],
+    ['bias_high_own_ses_college',
+        ['Likelihood of befriending peers with high SES',
+            'Student tendency to befriend other students with high-SES (a higher bias indicates that students are more likely to be friends with other students of high economic backgrounds)']],
+    ['bias_high_parent_ses_college',
+        ['Likelihood of befriending peers from high SES families',
+            'Students of high-SES families tendency to befriend other students who also have high-SES families (a higher bias indicates that students with families from higher economic backgrounds are more likely to befriend other students also from high-SES families)']],
+    ['clustering_college',
+        ['Friend cluster formation',
+            'The fraction of a student\'s individual friends that are also friends with each other']],
+    ['support_ratio_college',
+        ['% mutual friends',
+            'The proportion of friendship pairs that share another mutual friend within the same college']],
+    ['volunteering_rate_college',
+        ['Student volunteering rate',
+            'The percentage of Facebook users who are members of a group which is predicted to be about volunteering or activism based on group title and other group characteristics']]
 ]);
 
 class SmallMultiplesScatterplots {
@@ -72,28 +72,43 @@ class SmallMultiplesScatterplots {
     }
 
     initVis() {
-        plotOne = new ScatterplotOne(
-            {parentElement: '#scatterplot-one'},
+        plotOne = new SingleScatterplot(
+            {
+                parentElement: '#scatterplot-one',
+                plotIndex: 0,
+                backgroundColor: 'rgba(255, 193, 0, 0.5)',
+                circleClassName: 'point-one'
+            },
             dispatcher
         );
 
-        plotTwo = new ScatterplotTwo(
-            {parentElement: '#scatterplot-two'},
+        plotTwo = new SingleScatterplot(
+            {
+                parentElement: '#scatterplot-two',
+                plotIndex: 1,
+                backgroundColor: 'rgba(190, 190, 190, 0.65)',
+                circleClassName: 'point-two'
+            },
             dispatcher
-        );
+        )
 
-        plotThree = new ScatterplotThree(
-            {parentElement: '#scatterplot-three'},
+        plotThree = new SingleScatterplot(
+            {
+                parentElement: '#scatterplot-three',
+                plotIndex: 2,
+                backgroundColor: 'rgba(207, 128, 10, 0.6)',
+                circleClassName: 'point-three'
+            },
             dispatcher
-        );
+        )
     }
 
     updateVis() {
         let vis = this;
 
         // Figure out correlation (if it exists)
-        vis.correlationData = this.calculatePearsonCorrelation(vis.data);
-        vis.topCategories = this.getTopCategories(vis.correlationData);
+        vis.correlationData = vis.calculatePearsonCorrelation(vis.data);
+        vis.topCategories = vis.getTopCategories(vis.correlationData);
         vis.topThreeCategories = vis.topCategories.slice(0, 3).map(entry => entry && entry[0]);
 
         plotOne.data = vis.data;
@@ -115,7 +130,7 @@ class SmallMultiplesScatterplots {
         plotThree.updateVis();
     }
 
-        calculatePearsonCorrelation(data) {
+    calculatePearsonCorrelation(data) {
         let vis = this
         let correlationCoefficients = new Map();
 
